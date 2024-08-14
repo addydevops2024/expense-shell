0ad9293a9f3fa2beb41403a558fe7da638529bf4
Yashwanth <yrkunduru@joinbia.com>
2024-08-14 11:37:15 +0530

    update
    update

--- frontend.sh
+++ frontend.sh
@@ -1,1 +1,7 @@
-dnf install nginx -y 
+dnf install nginx -y && systemctl start nginx && systemctl enable nginx
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+cp expense.conf /etc/nginx/default.d/nginx.conf
+systemctl restart nginx
