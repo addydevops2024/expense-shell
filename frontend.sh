4938a9c702790289739c5226532f247b0dd550e9
Yashwanth <yrkunduru@joinbia.com>
2024-08-14 11:44:05 +0530

    update
    update

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,4 @@
-dnf install nginx -y && cp expense.conf /etc/nginx/default.d/nginx.conf && systemctl start nginx && systemctl enable nginx
+dnf install nginx -y && cp expense.conf /etc/nginx/default.d/expense.conf && systemctl start nginx && systemctl enable nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
